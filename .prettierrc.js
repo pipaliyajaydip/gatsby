1e78a896399a935f8e4bc9bdc71433fc8873cbfc
PIPALIYA JAYDIP <67597366+pipaliyajaydip@users.noreply.github.com>
2020-12-07 14:17:20 +0530

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -7,6 +7,7 @@ module.exports = {
       // package (used to parse jsdoc and provide content for API reference pages)
       // behaviour is inconsistent when not using semicolons after
       // object declarations.
+      // This file uses semicolons. It's needed here because `documentation`// This file uses semicolons. It's needed here because `documentation`
       files: ["**/api-node-helpers-docs.js"],
       options: {
         semi: true,
